ed2282883f49f4b423c4c97f64817185f1dc1ab6
aidaiym <omurbekovnaaidaiym@gmail.com>
2023-02-03 21:03:48 +0600

    second chapter

---
+++ integer_numbers/task_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int number, result;
+
+int main()
+{
+    cin >> number;
+    result = number * number;
+    cout << result;
+    return 0;
+}
