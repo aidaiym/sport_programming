42ed2b575bc6df384c1486db10f81ac3bb2d394a
aidaiym <omurbekovnaaidaiym@gmail.com>
2023-02-23 16:16:52 +0600

    added chess tasks

---
+++ conditional_operator/chess/task_1123.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+int a,b,c,d;
+cin>>a>>b>>c>>d;
+if(a+2==c&&b+1==d||a+1==c&&b+2==d||a-1==c&&b+2==d||a-2==c&&b+1==d||a-2==c&&b-1==d||a-1==c&&b-2==d||a+1==c&&b-2==d||a+2==c&&b-1==d)cout<<"YES";
+else cout<<"NO";
+}
