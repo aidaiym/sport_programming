ebaea6f13be282c8eee88c6a8bbdc33d3be24884
aidaiym <omurbekovnaaidaiym@gmail.com>
2023-04-28 15:28:25 +0600

    approximatw oopt

---
+++ olymp_KRSU/approximate_optimization.cpp
@@ -0,0 +1,22 @@
+/*
+
+Approximate Optimization
+Time Limit:	500 ms
+Memory Limit:	1024 kb
+Three famous scientists Voktaysed, Vokiruy and Veidafin decided to help well-known brainy creature Rotsor to pass exams. 
+Exams were on a very interesting and useful subject - Approximate Optimization. 
+The problem was to write a program that behaves like the one written by Cheetah (who gave only EXE file running away for playing Civilization III).
+ As Cheetah's mind is not so powerful Rotsor believes that Cheetah could write only a program evaluating some polynomial. 
+ He is also sure that the polynomial degree is at most four (5 is too much for Cheetah). 
+ Help the scientists to help Rotsor and write the desired program. 
+ Remember that Rotsor is too lazy and the scientists are too busy so the only hope is on you.
+
+Input
+On the first line of the input you will be given number of test cases T (no greater than 100). 
+T following lines will be numbers for evaluation (in range [-1000; 1000])
+
+Output
+Your program should produce T lines of output. Each line is the evaluation of the polynomial and should be same as the result of Cheetah's program. 
+As was already said Cheetah's program evaluates polynomial of the form f(x) = A*x^4 + B*x^3 + C*x^2 + D*x + E. 
+Print the values of f(x) with precision of 3 digits.
+*/ 
